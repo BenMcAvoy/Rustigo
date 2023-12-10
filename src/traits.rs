255d7253eb698bf1afe8a02603b43b836c622f37
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-10 23:02:18 +0000

    Setup traits

--- src/traits.rs
+++ src/traits.rs
@@ -3,17 +3,17 @@ use std::net::TcpStream;
 use std::sync::Arc;
 
 pub trait IntoArc {
-   fn into_arc(self) -> Arc<dyn Fn(TcpStream, Request) + Send + Sync + 'static>;
+    fn into_arc(self) -> Arc<dyn Fn(TcpStream, Request) + Send + Sync + 'static>;
 }
 
 impl<F: Fn(TcpStream, Request) + Send + Sync + 'static> IntoArc for F {
-   fn into_arc(self) -> Arc<dyn Fn(TcpStream, Request) + Send + Sync + 'static> {
-       Arc::new(self)
-   }
+    fn into_arc(self) -> Arc<dyn Fn(TcpStream, Request) + Send + Sync + 'static> {
+        Arc::new(self)
+    }
 }
 
 impl<F: Fn(TcpStream, Request) + Send + Sync + 'static> IntoArc for Arc<F> {
-   fn into_arc(self) -> Arc<dyn Fn(TcpStream, Request) + Send + Sync + 'static> {
-       self
-   }
+    fn into_arc(self) -> Arc<dyn Fn(TcpStream, Request) + Send + Sync + 'static> {
+        self
+    }
 }
