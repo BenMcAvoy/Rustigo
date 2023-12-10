6c00a72ff6d4e6b253ba4965d2957c60f9d0ae5d
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-10 22:19:28 +0000

    Remove traing semicolons

--- src/log.rs
+++ src/log.rs
@@ -2,18 +2,18 @@
 
 macro_rules! error {
    ($($arg:tt)*) => {
-       println!("\x1b[31m[ ERROR ]\x1b[0m {}", format_args!($($arg)*));
+       println!("\x1b[31m[ ERROR ]\x1b[0m {}", format_args!($($arg)*))
    };
 }
 
 macro_rules! info {
    ($($arg:tt)*) => {
-       println!("\x1b[32m[  INFO ]\x1b[0m {}", format_args!($($arg)*));
+       println!("\x1b[32m[  INFO ]\x1b[0m {}", format_args!($($arg)*))
    };
 }
 
 macro_rules! log {
    ($($arg:tt)*) => {
-       println!("\x1b[34m[  WARN ]\x1b[0m {}", format_args!($($arg)*));
+       println!("\x1b[34m[  WARN ]\x1b[0m {}", format_args!($($arg)*))
    };
 }
